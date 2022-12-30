abc02c6245b406842ae6cb90f1f6b1dea16c6c47
niris <niris@users.noreply.github.com>
2022-12-30 21:19:26 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react_ts_tuto
+# react_ts_tuto
+A simple webapp using React TypeScript, Vite, Redux
---
+++ src/components/AppHeader.tsx
@@ -0,0 +1,7 @@
+function AppHeader() {
+    return ( 
+        <h1>Hello App Header</h1>
+     );
+}
+
+export default AppHeader;
